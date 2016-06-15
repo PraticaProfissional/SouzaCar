ddd878023e61e4c4a07bc2d8720baa5213cf5fa7
Paulo César <pcmartins.santos@gmail.com>
2016-06-15 20:37:35 -0300

    CRUD Marca

---
+++ 4 - Implementação/4.1 - Codigo_Fonte/souzacar/src/main/java/br/com/souzacar/controller/MarcaController.java
@@ -0,0 +1,100 @@
+package br.com.souzacar.controller;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.souzacar.repository.Marca;
+import br.com.souzacar.repository.MarcaRepository;
+import br.com.souzacar.utlis.Response;
+
+@RestController
+@RequestMapping("/Marca")
+public class MarcaController {
+
+	@Autowired
+	private MarcaRepository marcaRepository;
+	@Autowired
+	private Response response;
+	@Autowired
+	private Marca marca;
+	
+	public MarcaController() {
+		super();
+	}
+
+	public MarcaController(MarcaRepository marcaRepository, Response response, Marca marca) {
+		super();
+		this.marcaRepository = marcaRepository;
+		this.response = response;
+		this.marca = marca;
+	}
+
+	@CrossOrigin(value = "*")
+	@RequestMapping(value = "/marca/", method = RequestMethod.POST)
+	public @ResponseBody Response insert(@RequestBody Marca marca){
+		try {
+			marcaRepository.save(marca);
+			response.setData(marca);
+		} catch (Exception e) {
+			response.setStatus(500, e.getMessage());
+		}
+		return response;
+	}
+	
+	@CrossOrigin(value = "*")
+	@RequestMapping(value = "/marca/{id}", method = RequestMethod.PUT)
+	public @ResponseBody Response update(@PathVariable long id, @RequestBody Marca marca){
+		try {
+			marcaRepository.save(marca);
+			response.setData(marca);
+		} catch (Exception e) {
+			response.setStatus(500, e.getMessage());
+		}
+		
+		return response;
+	}
+	
+	@CrossOrigin(value = "*")
+	@RequestMapping(value = "/marca/{id}", method = RequestMethod.DELETE)
+	public @ResponseBody Response delete(@PathVariable long id){
+		marca = marcaRepository.findOne(id);
+		try {
+			marcaRepository.delete(marca);
+		} catch (Exception e) {
+			response.setStatus(500, e.getMessage());
+		}
+		
+		return new Response(marca, null);
+	}
+	
+	@CrossOrigin(value = "*")
+	@RequestMapping(value = "/marca/list", method = RequestMethod.GET)
+	public Response list(){
+		List<Marca> result = new ArrayList<Marca>();
+		Iterator<Marca> iterator = marcaRepository.findAll().iterator();
+		
+		while(iterator.hasNext()){
+			result.add(iterator.next());
+		}
+		
+		return new Response(marca, null);
+	}
+	
+	@CrossOrigin(value = "*")
+	@RequestMapping(value = "/marca/{id}", method = RequestMethod.GET)
+	public Response get(@PathVariable long id){
+		marca = marcaRepository.findOne(id);
+		
+		return new Response(marca, null);
+	}
+}
